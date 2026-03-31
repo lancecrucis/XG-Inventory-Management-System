bbc919a822252eb3008a370641c1774293b90a78
Lance <lancecrucis123@gmail.com>
2026-03-31 18:54:08 +0800

    fix: track frontend files properly

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import LoginPage from './LoginPage'
+
+function App() {
+
+  return (
+    <>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
